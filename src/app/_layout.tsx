e51e7c4fd9fcbf43b4bffd0f9bc5859ab8fec9b3
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-04-14 16:44:49 +0530

    more refined structure

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,12 @@
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-import LoadingScreen from "@/screens/LoadingScreen";
-
 // Initialize theme
 import "@/config/theme/initTheme";
-import RootLayout from "@/components/layout/RootLayout";
 import { useFonts } from "@expo-google-fonts/montserrat";
 import fonts from "@/config/theme/fonts";
-
-import { useSessionStore } from "@/store/SessionStore";
-import { supabase } from "@/lib/supabase";
-import { Redirect, router, Slot, useSegments } from "expo-router";
+import { Slot } from "expo-router";
+import AuthHandler from "@/components/handlers/AuthHandler";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -24,35 +19,10 @@ export default function _layout() {
     }
   }, [fontsLoaded]);
 
-  // Supabase Login
-  const segments = useSegments();
-
-  const { isInitializing, getSession, session, setSession, setIsInitializing } =
-    useSessionStore();
-
-  useEffect(() => {
-    getSession();
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      console.log("auth state changed: ", session);
-      setSession(session);
-      setIsInitializing(false);
-    });
-  }, []);
-
-  console.log(session, isInitializing);
-
-  useEffect(() => {
-    if (isInitializing) return;
-
-    const inTabsGroup = segments[0] === "(tabs)";
-    if (session && !inTabsGroup) router.replace("/(tabs)");
-    else if (!session) router.replace("/(auth-screens)/signin");
-  }, [session, isInitializing]);
-
-  // if (isInitializing || !fontsLoaded) return <LoadingScreen />;
-
-  // if (session) <Redirect href={"/(tabs)"} />;
-
-  return <Slot />;
+  return (
+    <>
+      <Slot />
+      <AuthHandler />
+    </>
+  );
 }
---
+++ src/components/handlers/AuthHandler.tsx
@@ -0,0 +1,34 @@
+import { useSessionStore } from "@/store/SessionStore";
+import { supabase } from "@/lib/supabase";
+import { router, useSegments } from "expo-router";
+import { useEffect } from "react";
+
+export default function AuthHandler() {
+  // Supabase Login
+  const segments = useSegments();
+
+  const { isInitializing, getSession, session, setSession, setIsInitializing } =
+    useSessionStore();
+
+  useEffect(() => {
+    getSession();
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log("auth state changed: ", session);
+      setSession(session);
+      setIsInitializing(false);
+    });
+  }, []);
+
+  console.log(session, isInitializing);
+
+  useEffect(() => {
+    if (isInitializing) return;
+
+    const inTabsGroup = segments[0] === "(tabs)";
+    if (session && !inTabsGroup) router.replace("/(tabs)");
+    else if (!session) router.replace("/(auth-screens)/signin");
+  }, [session, isInitializing]);
+
+  return null;
+}
