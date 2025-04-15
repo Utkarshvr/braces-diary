eb59a3d2e8c0573ae9fc941a2d3ddfda3b1cfacf
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-04-15 20:57:13 +0530

    calendar setup done

--- src/components/ui/calendar/CalendarView.tsx
+++ src/components/ui/calendar/CalendarView.tsx
@@ -1,10 +1,36 @@
-import { View } from "react-native";
-import { Calendar } from "react-native-ui-lib/src/incubator";
+import { View, Colors } from "react-native-ui-lib";
+import { useAppearanceSchemeStore } from "@/store/AppearanceSchemeStore";
+import MyCalendar from "./MyCalendar";
 
 export default function CalendarView() {
+  // TODO: Fix theme being still
+  const { colorScheme } = useAppearanceSchemeStore();
+  console.log("Rerendered: ", colorScheme);
+
   return (
     <View>
-      <Calendar data={[{ start: 1, end: 2, id: "2" }]} />
+      <MyCalendar
+        hideExtraDays
+        theme={{
+          // Background & Text Color
+          calendarBackground: Colors.$backgroundDefault,
+          dayTextColor: Colors.$textDefault,
+          monthTextColor: Colors.$textDefault,
+          arrowColor: Colors.$textDefault,
+          selectedDayBackgroundColor: Colors.$textDefault,
+
+          selectedDayTextColor: "#000",
+          todayBackgroundColor: Colors.$backgroundNeutral,
+
+          todayTextColor: "#00adff",
+
+          // Font Family
+          textDayFontFamily: "montRegular",
+          textMonthFontFamily: "montBold",
+          todayButtonFontFamily: "montRegular",
+          textDayHeaderFontFamily: "montRegular",
+        }}
+      />
     </View>
   );
 }
---
+++ src/components/ui/calendar/MyCalendar.tsx
@@ -0,0 +1,5 @@
+import { Calendar, CalendarProps } from "react-native-calendars";
+
+export default function MyCalendar(props: CalendarProps) {
+  return <Calendar {...props} />;
+}
